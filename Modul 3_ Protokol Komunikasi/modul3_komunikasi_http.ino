47112382ed34dd631187d90336bd3206a3297c10
MRaihan-XXL <m.raihan.izudin@gmail.com>
2026-09-22 11:53:57 +0700

    update

--- Modul 3_ Protokol Komunikasi/modul3_komunikasi_http.ino
+++ Modul 3_ Protokol Komunikasi/modul3_komunikasi_http.ino
@@ -3,42 +3,35 @@
 #include <WiFiClientSecureBearSSL.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "Nxxy";
-const char* password = "Nuxxccyy";
+const char* ssid = "OPPO A31";
+const char* password = "12345678";
 const char* serverUrl = "https://httpbin.org/post";
 
 void setup() {
   Serial.begin(115200);
-
   WiFi.begin(ssid, password);
 
   Serial.print("Menghubungkan ke WiFi");
-
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println();
   Serial.println("WiFi berhasil terhubung!");
 }
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-
     // Membuat koneksi HTTPS
     std::unique_ptr<BearSSL::WiFiClientSecure> client(
       new BearSSL::WiFiClientSecure
     );
-
     // Mengabaikan sertifikat SSL
     client->setInsecure();
-
     HTTPClient http;
 
     // Memulai koneksi ke server
     if (http.begin(*client, serverUrl)) {
-
       http.addHeader("Content-Type", "application/json");
 
       // Membuat objek data sensor dalam format JSON
@@ -58,20 +51,16 @@ void loop() {
       if (httpResponseCode > 0) {
         Serial.print("Kode Response HTTP: ");
         Serial.println(httpResponseCode);
-
         Serial.println("Isi Response:");
         Serial.println(http.getString());
       } else {
         Serial.print("Pengiriman gagal, kode error: ");
         Serial.println(httpResponseCode);
       }
-
       http.end();
-
     } else {
       Serial.println("Gagal memulai koneksi HTTP");
     }
   }
-
   delay(10000); // kirim data setiap 10 detik
 }
--- Modul 3_ Protokol Komunikasi/modul3_komunikasi_mqtt.ino
+++ Modul 3_ Protokol Komunikasi/modul3_komunikasi_mqtt.ino
@@ -1,76 +1,70 @@
-// Pin LED
-int led1 = 2;
-int led2 = 3;
-int led3 = 4;
-int led4 = 5;
-int led5 = 6;
-int led6 = 7;
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "OPPO A31";
+const char* password = "12345678";
+
+const char* mqttServer = "broker.hivemq.com";
+const int mqttPort = 1883;
+
+// Ganti dengan topic unik kelompok kamu
+const char* mqttTopic = "unsoed/tk245004/kelompok3/sensor";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void hubungkanWiFi() {
+  WiFi.begin(ssid, password);
+  Serial.print("Menghubungkan ke WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi berhasil terhubung!");
+}
+
+void hubungkanMQTT() {
+  while (!client.connected()) {
+    Serial.print("Menghubungkan ke broker MQTT...");
+    String clientId = "ESP8266Client-" + String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("berhasil terhubung!");
+    } else {
+      Serial.print("gagal, rc=");
+      Serial.print(client.state());
+      Serial.println(" coba lagi dalam 2 detik");
+      delay(2000);
+    }
+  }
+}
 
 void setup() {
-  // Menentukan semua pin LED sebagai OUTPUT
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(led5, OUTPUT);
-  pinMode(led6, OUTPUT);
-
-  // Memulai komunikasi Serial
-  Serial.begin(9600);
-
-  Serial.println("=== KONTROL 6 LED ===");
-  Serial.println("Masukkan angka 1 - 6");
+  Serial.begin(115200);
+  hubungkanWiFi();
+  client.setServer(mqttServer, mqttPort);
 }
 
 void loop() {
-
-  // Mengecek apakah ada data dari Serial Monitor
-  if (Serial.available() > 0) {
-
-    int pilihan = Serial.parseInt();
-
-    // Matikan semua LED terlebih dahulu
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
-    digitalWrite(led4, LOW);
-    digitalWrite(led5, LOW);
-    digitalWrite(led6, LOW);
-
-    // Percabangan
-    if (pilihan == 1) {
-      digitalWrite(led1, HIGH);
-      Serial.println("LED 1 MENYALA");
-    }
-    
-    else if (pilihan == 2) {
-      digitalWrite(led2, HIGH);
-      Serial.println("LED 2 MENYALA");
-    }
-    
-    else if (pilihan == 3) {
-      digitalWrite(led3, HIGH);
-      Serial.println("LED 3 MENYALA");
-    }
-    
-    else if (pilihan == 4) {
-      digitalWrite(led4, HIGH);
-      Serial.println("LED 4 MENYALA");
-    }
-    
-    else if (pilihan == 5) {
-      digitalWrite(led5, HIGH);
-      Serial.println("LED 5 MENYALA");
-    }
-    
-    else if (pilihan == 6) {
-      digitalWrite(led6, HIGH);
-      Serial.println("LED 6 MENYALA");
-    }
-    
-    else {
-      Serial.println("Pilihan tidak valid!");
-      Serial.println("Masukkan angka 1 - 6.");
-    }
+  if (!client.connected()) {
+    hubungkanMQTT();
   }
+  client.loop();
+
+  // Membuat data sensor dalam format JSON
+  JsonDocument doc;
+  doc["suhu"] = 28.5;
+  doc["kelembaban"] = 65.0;
+
+  char buffer[128];
+  serializeJson(doc, buffer);
+
+  // Mempublikasikan data ke topic MQTT
+  client.publish(mqttTopic, buffer);
+  Serial.print("Data terkirim ke topic ");
+  Serial.print(mqttTopic);
+  Serial.print(": ");
+  Serial.println(buffer);
+
+  delay(5000); // publish data setiap 5 detik
 }
